c7aafccdf44eefcc94b6f8b9826fbb2177c1793a
Felice Nicolas Barcellona <felicebarcellona1@gmail.com>
2023-03-15 17:04:04 +0100

    fix

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const form = document.querySelector("form");
 const email = document.getElementById("mail");
 const errorMessage = document.querySelector(".error-message");
-const inputContainer = document.querySelector(".input-container")
+const inputContainer = document.querySelector(".input-container");
+const desktop = window.matchMedia("(min-width: 1024px)");
+const tablet = window.matchMedia("(max-width: 768px)")
 const emailRegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  if(email.value === '' || email.value !== '' && !emailRegExp.test(email.value)){
+  if((email.value === '' && desktop.matches )||(email.value !== '' && !emailRegExp.test(email.value) && desktop.matches)){
+    email.className = 'invalid';
+    errorMessage.textContent = 'Please provide a valid email address';
+  } else if((email.value === '' && tablet.matches )||(email.value !== '' && !emailRegExp.test(email.value) && tablet.matches)){
     email.className = 'invalid';
     errorMessage.textContent = 'Please provide a valid email address';
     inputContainer.style.marginBottom = '49px';
